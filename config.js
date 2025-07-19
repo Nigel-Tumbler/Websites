64c895af048334d2fdaba9bfd31350aeae325c59
Nigel-Tumbler <kudzanaitambs92@gmail.com>
2025-07-19 14:53:59 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+
+
+ let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";
+let apiKey = checkApiKey();
+
+function checkApiKey() {
+    const key = localStorage.getItem("apiKey");
+
+    if (!key) {
+        window.open("enter-api-key.html", "_self");
+    }
+
+    return key;
+}
